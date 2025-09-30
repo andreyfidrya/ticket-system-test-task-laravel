a7024b2a4ac10c106957581a9fa97edeb71cc786
Andrey Fidrya <findhostcoupons@gmail.com>
2025-09-30 17:20:12 +0300

    installing spatie laravel medialibrary

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Ticket extends Model
+class Ticket extends Model implements HasMedia
 {
-    use HasFactory;
+    use InteractsWithMedia;
     
     protected $fillable = [
         'customer_id',
@@ -16,6 +18,11 @@ class Ticket extends Model
         'status',
         'response_date',
     ];
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('attachments')->useDisk('public');
+    }
 
     public function customer()
     {
