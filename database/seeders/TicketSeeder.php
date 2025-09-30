b9e3e9797c4ae46a4389b3babb7ff23cf7b40e23
Andrey Fidrya <findhostcoupons@gmail.com>
2025-09-30 21:34:05 +0300

    TicketSeeder

--- database/seeders/TicketSeeder.php
+++ database/seeders/TicketSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Ticket;
+use Carbon\Carbon;
 
 class TicketSeeder extends Seeder
 {
@@ -12,6 +13,28 @@ class TicketSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Ticket::create([
+            'customer_id' => 1,
+            'topic' => 'Проблема с оплатой',
+            'text' => 'Клиент сообщил, что не прошла оплата через систему.',
+            'status' => 'новый',
+            'response_date' => NULL,
+        ]);
+
+        Ticket::create([
+            'customer_id' => 2,
+            'topic' => 'Вопрос по доставке',
+            'text' => 'Клиент интересуется сроками доставки.',
+            'status' => 'в работе',
+            'response_date' => Carbon::now()->subDays(2),
+        ]);
+
+        Ticket::create([
+            'customer_id' => 3,
+            'topic' => 'Техническая ошибка',
+            'text' => 'Клиент сообщает об ошибке на сайте при входе.',
+            'status' => 'обработан',
+            'response_date' => Carbon::now()->subDay(),
+        ]);
     }
 }
