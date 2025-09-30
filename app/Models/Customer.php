ece29de4175e92a2457854dad025328e2cdd4892
Andrey Fidrya <findhostcoupons@gmail.com>
2025-09-30 16:34:19 +0300

    creating model + migration for Ticket

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',        
+    ];
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
