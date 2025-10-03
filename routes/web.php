db0355ae7bcf21a0a30cb1b68102fa34b448f56c
Andrey Fidrya <findhostcoupons@gmail.com>
2025-10-03 17:51:10 +0300

    installing laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WidgetController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::get('/widget', [WidgetController::class, 'index'])->name('widget.form');
 Route::post('/widget/send', [WidgetController::class, 'send'])->name('widget.send');
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
